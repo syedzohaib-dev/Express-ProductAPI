5258ff2084f783a854fa49c6e192db8d0d6d273c
syed zohaib <zohaibsyed914@gmail.com>
2023-08-23 15:58:18 +0500

    Ready

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const port = process.env.SERVER_PORT
 const cors = require('cors')
-// const path = require('path')
+const path = require('path')
+
+
+const clientPath = path.join(__dirname, './client/dist')
+app.use('/', express.static(clientPath))
 
 app.use(express.json()) // stringify me change karnay k liye ye karyngy json ko
 app.use(cors())
@@ -18,6 +22,10 @@ app.use('/api', require('./API/Products/productRoutes'))
 app.use('/api', require('./API/mailer/router'))
 app.use('/api', require('./API/orders/router'))
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'))
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on portnpm  ${port}`)
+  console.log(`Example app listening on portnpm  http://localhost:${port}`)
 })
